5ba7f38d0eca63da7969f2b76e49f080ccc0875f
Koichi <cx.w0077@gmail.com>
2016-02-20 20:37:49 +0800

    Movie再生機能追加

---
+++ seconds/AVPlayerView.swift
@@ -0,0 +1,35 @@
+//
+//  AVPlayerView.swift
+//  seconds
+//
+//  Created by 高城晃一 on 2016/02/20.
+//  Copyright © 2016年 KoichiTakashiro. All rights reserved.
+//
+
+import AVFoundation
+import UIKit
+
+class AVPlayerView: UIView {
+    
+    // レイヤーをAVPlayerLayerにする為のラッパークラス.
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    override class func layerClass() -> AnyClass{
+        return AVPlayerLayer.self
+    }
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
